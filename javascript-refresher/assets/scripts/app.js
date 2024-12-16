b2fd1e754364c7e96935982877f2113b006453da
SDhanush163 <sdhanush163@gmail.com>
2024-12-16 13:52:00 +0530

    Classes and Arrays

--- javascript-refresher/assets/scripts/app.js
+++ javascript-refresher/assets/scripts/app.js
@@ -11,13 +11,16 @@ Import and Export
 // console.log(apiKeyDefault);
 
 // Importing as an object from util.js
-import * as util from './util.js';
-console.log(util.someData);
+import * as util from "./util.js";
+console.log(util.apiKey);
+util.utilGreet();
+const user = new util.User("Ash", 10);
+console.log(user);
 
 /*************************
 Values and Variables
 *************************/
-const helloMessage = 'Hello world'; // Cannot be changed, read-only
+const helloMessage = "Hello world"; // Cannot be changed, read-only
 let usermessage = "New Value"; // Can be changed
 
 /*************************
@@ -25,34 +28,71 @@ Operators
 *************************/
 
 console.log(10 + 5);
-console.log('Hello' + 'world');
+console.log("Hello" + "world");
 console.log(10 === 5);
 
 /*************************
 Functions and Parameters
 *************************/
 
-// Function definition; () contains parameters. 
-// Func can have as many parameters as needed. 
+// Function definition; () contains parameters.
+// Func can have as many parameters as needed.
 // Parameters can have default value eg message
 // Function can perform a task and print/return the final result
-function greetUser (userName, message = "Hello") { 
-    console.log('Hello ' + userName + '! ' + message);
+function greetUser(userName, message = "Hello") {
+    console.log("Hello " + userName + "! " + message);
     // return username + message;
 }
-greetUser('Alan', "Lets catch em all"); // Can be recalled as many times as needed
+greetUser("Alan", "Lets catch em all"); // Can be recalled as many times as needed
 
-function createGreeting (userName, message) {
-    return 'Hi I am ' + userName + '. ' + message;
+function createGreeting(userName, message) {
+    return "Hi I am " + userName + ". " + message;
 }
-let greeting = createGreeting('Max', ' Awesome!')
-console.log(greeting)
+let greeting = createGreeting("Max", " Awesome!");
+console.log(greeting);
 
 function combine(a, b, c) {
-    let result = a * b / c;
+    let result = (a * b) / c;
     return result;
 }
 
 // Arrow function
-const btn = document.getElementById('btn');
-btn.onclick = () => console.log('clicked');
+const btn = document.getElementById("btn");
+btn.onclick = () => console.log("clicked");
+
+/*************************
+Objects
+*************************/
+
+import { User } from "./util.js";
+const user1 = new User("Mark", 29);
+console.log(user1);
+user1.greet();
+
+/*************************
+Arrays and Array methods
+*************************/
+
+const hobbies = ["Sports", "Cooking", "Reading"];
+console.log(hobbies[1]);
+hobbies.push("Swimming");
+console.log(hobbies);
+
+// Finding the index of the required value in the array
+const index = hobbies.findIndex((item) => item === "Cooking");
+console.log("Index = " + index);
+
+// Map the array to a unordered list
+const hobbyList = document.getElementById("hobby");
+hobbies.map((item) => {
+    let li = document.createElement("li");
+    li.innerHTML = item;
+    hobbyList.appendChild(li);
+});
+
+// convertt the array of strings to array of objects with text and index
+const editHobbies = hobbies.map((item) => ({
+    text: item,
+    index: hobbies.findIndex((item2) => item2 == item),
+}));
+console.log(editHobbies);
--- javascript-refresher/assets/scripts/util.js
+++ javascript-refresher/assets/scripts/util.js
@@ -1,3 +1,23 @@
-export default 'testDefault';
-export let apiKey = 'testApiKey';
+// Exporting default value
+export default "testDefault";
+
+// Exporting values
+export let apiKey = "testApiKey";
 export let someData = 10;
+
+// Exporting function
+export function utilGreet() {
+    console.log("Greetings from Util method");
+}
+
+// Exporting class
+export class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet() {
+        console.log("Hi!");
+    }
+}

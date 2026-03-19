991480f237938eac782843523164c194759f8a28
Dhanush <sdhanush163@gmail.com>
2026-03-19 12:41:38 +0530

    Player setup

--- time-challenge/src/components/Player.jsx
+++ time-challenge/src/components/Player.jsx
@@ -1,11 +1,33 @@
-export default function Player() {
+import { useRef, useState } from "react";
+
+const toSentenceCase = (str) => {
+  if (!str) return "";
+  str = str.toLowerCase();
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+const Player = () => {
+  const [playerName, setPlayerName] = useState(null);
+  const inputValue = useRef();
+
+  const handleClick = () => {
+    setPlayerName(
+      inputValue.current.value
+        ? toSentenceCase(inputValue.current.value)
+        : null,
+    );
+    inputValue.current.value = "";
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={inputValue} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
-}
+};
+
+export default Player;

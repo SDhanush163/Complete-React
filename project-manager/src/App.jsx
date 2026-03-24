725b084fcf987d643f457b0310667d649cf7560e
Dhanush <sdhanush163@gmail.com>
2026-03-24 08:43:44 +0530

    Switching between landing page and new project

--- project-manager/src/App.jsx
+++ project-manager/src/App.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    projectId: undefined,
+    projects: [],
+  });
+  let content;
+
+  const handleAddProject = () => {
+    setProjectState((prev) => {
+      return { ...prev, projectId: null };
+    });
+  };
+
+  if (projectState.projectId === null) content = <NewProject />;
+  else if (projectState.projectId === undefined)
+    content = <NoProjectSelected onStartProject={handleAddProject} />;
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NoProjectSelected />
+      <Sidebar onStartProject={handleAddProject} />
+      {content}
     </main>
   );
 }
--- project-manager/src/components/Sidebar.jsx
+++ project-manager/src/components/Sidebar.jsx
@@ -1,13 +1,13 @@
 import Button from "./Button";
 
-const Sidebar = () => {
+const Sidebar = ({ onStartProject }) => {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 rounded-r-xl md:w-72">
       <h2 className="mb-8 font-bold uppercase text-stone-200 md:text-xl">
         Your Projects
       </h2>
       <div>
-        <Button>+ Add Project</Button>
+        <Button onClick={onStartProject}>+ Add Project</Button>
       </div>
     </aside>
   );

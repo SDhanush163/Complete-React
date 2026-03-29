19ae7a172c608e13e34e9463f2ab6937753f9067
Dhanush <sdhanush163@gmail.com>
2026-03-29 14:22:24 +0530

    Update App.jsx

--- project-manager/src/App.jsx
+++ project-manager/src/App.jsx
@@ -4,6 +4,34 @@ import NoProjectSelected from "./components/NoProjectSelected";
 import Sidebar from "./components/Sidebar";
 import SelectedProject from "./components/SelectedProject";
 
+/* --------------------------------------------------------
+App - Base point of the Project management application
+  - project state - 
+    1. ID > unique identifier for each project being tracked
+      undefined > landing page
+      null > new project definition
+      project ID > open the selected project 
+    2. projects > List of all projects. Each project will have 
+      a. Title of the project
+      b. Decription of the project
+      c. Due date by when the project was to be completed
+    3. tasks > All tasks associated to each prject. Each task
+      will have
+      a. Task ID
+      b. Associated project ID
+      c. task details
+    4. selectedProject > 
+
+    - handleStartProject > Open the new project page
+    - handleAddProject > Add a new project
+    - handleCancelStartProject > Cancel the new project and 
+        return to old screen
+    - handleSelectProject > Open a selected project
+    - handleDeleteProject > Delete the selected project
+    - handleAddTask > Add a task to the selected project
+    - handleDeleteTask > Delete the highlighted task
+-------------------------------------------------------- */
+
 function App() {
   const [projectState, setProjectState] = useState({
     projectId: undefined,

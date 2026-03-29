5b3c52c4c5e59b312918faab631bf6d8e2c86b66
Dhanush <sdhanush163@gmail.com>
2026-03-29 16:34:28 +0530

    Created context for the cart

--- elegant-context-shop/src/components/Product.jsx
+++ elegant-context-shop/src/components/Product.jsx
@@ -1,24 +1,24 @@
-export default function Product({
-  id,
-  image,
-  title,
-  price,
-  description,
-  onAddToCart,
-}) {
+import React, { useContext } from "react";
+import { CartContext } from "../store/cartContext";
+
+const Product = () => {
+  const { addToCart } = useContext(CartContext);
+
   return (
     <article className="product">
       <img src={image} alt={title} />
       <div className="product-content">
         <div>
           <h3>{title}</h3>
-          <p className='product-price'>${price}</p>
+          <p className="product-price">${price}</p>
           <p>{description}</p>
         </div>
-        <p className='product-actions'>
-          <button onClick={() => onAddToCart(id)}>Add to Cart</button>
+        <p className="product-actions">
+          <button onClick={() => addToCart(id)}>Add to Cart</button>
         </p>
       </div>
     </article>
   );
-}
+};
+
+export default Product;
--- elegant-context-shop/src/components/Shop.jsx
+++ elegant-context-shop/src/components/Shop.jsx
@@ -1,9 +1,12 @@
-export default function Shop({ children }) {
+import React from "react";
+
+const Shop = () => {
   return (
     <section id="shop">
       <h2>Elegant Clothing For Everyone</h2>
-
       <ul id="products">{children}</ul>
     </section>
   );
-}
+};
+
+export default Shop;

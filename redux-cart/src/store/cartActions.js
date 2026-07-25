76a7bb8957219d181c21bf3630bfc075e5e41747
Dhanush <sdhanush163@gmail.com>
2026-07-25 22:02:45 +0530

    Firebase setup steps

--- redux-cart/src/store/cartActions.js
+++ redux-cart/src/store/cartActions.js
@@ -1,6 +1,55 @@
 import { replaceCart } from "./cartSlice";
 import { showNotification } from "./uiSlice";
 
+/* ===============================================================================
+FIREBASE REALTIME DATABASE SETUP (TESTING MODE)
+==================================================================================
+
+1. Go to:
+   https://console.firebase.google.com/
+
+2. Create a new Firebase Project
+   - Click "Create a Project"
+   - Enter a project name
+   - Complete the project setup
+
+3. Enable Realtime Database
+   - In the left sidebar, go to:
+       Build → Realtime Database
+   - Click "Create Database"
+
+4. Choose the Database Location
+   - Select the region closest to you
+   - Click "Next"
+
+5. Select Security Rules
+   - Choose "Start in TEST MODE"
+   - Click "Enable"
+
+6. Copy the Database URL
+   It will look similar to:
+
+   https://your-project-id-default-rtdb.firebaseio.com
+
+7. Replace <backend_url> in this file with your database URL.
+
+   Example:
+
+   fetch("https://your-project-id-default-rtdb.firebaseio.com/cart.json")
+
+   Note:
+   - Do NOT include a trailing slash in <backend_url>.
+   - Always end Firebase requests with ".json".
+
+8. Run the application.
+   Firebase will automatically create the "cart" node the first
+   time data is sent using the PUT request.
+
+9. Before deploying your application,
+   change the database rules from TEST MODE to secure rules.
+
+=============================================================================== */
+
 export const fetchCartData = () => {
   return async (dispatch) => {
     const fetchData = async () => {
